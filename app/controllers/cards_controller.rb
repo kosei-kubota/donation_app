a35da65e4ddec1c681cb012fce2ceca61197222d
kosei-kubota <kagesame1096@gmail.com>
2021-03-13 15:59:01 +0900

    カード登録後のリダイレクト設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,10 +9,16 @@ class CardsController < ApplicationController
     card: params[:card_token]
     )
 
-    card = Card.new(
-      card_token: params[:card_token],
-      customer_token: customer.id,
-      user_id: current_user.id
-    )
+  card = Card.new(
+    card_token: params[:card_token],
+    customer_token: customer.id,
+    user_id: current_user.id
+  )
+
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to :new
+    end
   end
 end
