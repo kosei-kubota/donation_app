24223aa1cdf27649c971851a30786c663ac05872
kosei-kubota <kagesame1096@gmail.com>
2021-03-13 15:47:32 +0900

    カードトークンと顧客トークンをデータベースに保存する実装

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,12 @@
 class CardsController < ApplicationController
   def new
   end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create(
+    description: 'test',
+    card: params[:card_token]
+    )
+  end
 end
