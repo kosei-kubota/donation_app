0463730d1da12ecf0081bb481c074604bf39d36f
kosei-kubota <kagesame1096@gmail.com>
2021-03-13 15:36:12 +0900

    カードオブジェクトの生成

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,5 +3,15 @@ const pay = () => {
   const form = document.getElementById("charge-form"); 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+  
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
   });
  };
