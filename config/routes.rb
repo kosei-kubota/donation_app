19d2a465e1f334d15bc8e5570a137e208108dd07
kosei-kubota <kagesame1096@gmail.com>
2021-03-13 14:53:28 +0900

    カード登録画面のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root 'donations#index'
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
 end
