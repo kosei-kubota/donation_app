f6520c44e30592aa129ee2bf298a743a0132a637
kosei-kubota <kagesame1096@gmail.com>
2021-03-14 10:54:46 +0900

    カード情報を元に顧客情報の取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def show
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     card = Card.find_by(user_id: current_user.id)
+
+    customer = Payjp::Customer.retrieve(card.customer_token)
+    @card = customer.cards.first
   end
 
   def update
