dd8e0c20f24150beb2bb6964e147dfdfe6db3c41
kosei-kubota <kagesame1096@gmail.com>
2021-03-14 13:38:21 +0900

    PAY.JPに支払い情報を送信実装

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -7,6 +7,14 @@ class DonationsController < ApplicationController
 
   def order
     redirect_to new_card_path and return unless current_user.card.present?
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer_token = current_user.card.customer_token
+    Payjp::Charge.create(
+      amount: @donation.price,
+      customer: customer_token,
+      currency: 'jpy'
+      )
   end
 
   private
