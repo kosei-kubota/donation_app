81218129e0d7b849315a8bbc6fdf13e6a498ce8b
kosei-kubota <kagesame1096@gmail.com>
2021-03-14 13:25:10 +0900

    寄付する内容を特定

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,16 @@
 class DonationsController < ApplicationController
+  before_action :find_donation, only: :order
+
   def index
     @donations = Donation.all
   end
+
+  def order
+  end
+
+  private
+  def find_donation
+    @donation = Donation.find(params[:id])
+  end
+  
 end
